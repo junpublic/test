6ca6fad78864a90acfb3fdfb9ef67be1e2504a50
Canon <Canon@DESKTOP-PL2F7PK>
2020-05-06 20:59:35 +0900

    0506

--- WebUI/WebContent/4_javascript_class/06_function/js/arg_func.js
+++ WebUI/WebContent/4_javascript_class/06_function/js/arg_func.js
@@ -1,14 +1,14 @@
 // 함수의 인자로 들어오는 함수처리
-function arrayProcess( data, f ) {
+function arrayProcess( data, f ) {//f로 함수이름이 넘어옴
 	for( key=0; key < data.length; key++ ){
 	//for(var key in data ){
-		f( key, data[key].value );
+		f( key, data[key].value );//f에 함수명을 넣어주면 밑에 미리 지정해놓은 함수가 불려져옴 개쩌는 기능인듯 
 	}
 }
 
 // 각각의 데이터를 더하는 함수
 var sumResult = 0;
-function sum( key, data ){	
+function sum( key, data ){	//현재 key는 없어도됨
 	sumResult += parseInt(data);
 }
 
